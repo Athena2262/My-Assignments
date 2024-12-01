d6c74c5b6b39d36626c545e83f6bc929c6fba6dc
Nishanthini S <93366563+Athena2262@users.noreply.github.com>
2024-12-01 13:42:29 +0530

    OddIndexToUppercase.java

--- week3.assignment/String/OddIndexToUppercase.java
+++ week3.assignment/String/OddIndexToUppercase.java
@@ -1,1 +1,26 @@
+package week3.assignment;
+
+public class OddIndexToUppercase {
+
+	public static void main(String[] args) {
+		// Input string
+        String test = "changeme";
+
+        // Convert the string to a character array
+        char[] odd = test.toCharArray();
+
+        // Iterate through the characters from end to start
+        for (int i = 0; i < odd.length; i++) {
+            // Check if the index is odd
+            if (i % 2 != 0) {
+                // Change the character at the odd index to uppercase
+                odd[i] = Character.toUpperCase(odd[i]);
+            }
+        }
+        String result = new String(odd);
+        System.out.println(result);
+
+	}
+
+}
 
