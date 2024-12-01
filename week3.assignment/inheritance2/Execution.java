4f6b9bea4bd55384f668b4fc7f66ace08f7811b7
Nishanthini S <93366563+Athena2262@users.noreply.github.com>
2024-12-01 13:34:18 +0530

    Execution.java

---
+++ week3.assignment/inheritance2/Execution.java
@@ -0,0 +1,15 @@
+package week3.assignment;
+
+public class Executionclass extends Button{
+	public static void main(String[] args) {
+		Executionclass ex = new Executionclass();
+		ex.click();
+		ex.setText();
+		ex.submit();
+		CheckBox chbx = new CheckBox();
+		chbx.checkbutton();
+		Radiobutton rbn = new Radiobutton();
+		rbn.selectRadioButton();
+	}
+
+}
