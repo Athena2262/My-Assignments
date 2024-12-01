6c98090d451707caeb2d31d0fe022675c34ee399
Nishanthini S <93366563+Athena2262@users.noreply.github.com>
2024-12-01 13:24:45 +0530

    testdata.java

---
+++ week3.assignment/inheritance1/testdata.java
@@ -0,0 +1,11 @@
+package week3.assignment;
+
+public class TestData {
+	public void entercredentials() {
+		System.out.println("Credentials");
+	}
+	public void navigatetohomepage() {
+		System.out.println("Navigate");
+	}
+
+}
